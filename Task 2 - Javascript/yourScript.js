27cacb7d1455735a34fc97b51298dd07aac481b8
unknown <miller.carmen96@gmail.com>
2019-04-27 02:28:09 +0300

    Best calculator done

--- Task 2 - Javascript/yourScript.js
+++ Task 2 - Javascript/yourScript.js
@@ -1,1 +1,20 @@
-// Your code goes in this file!
+var number = 0; 
+document.querySelector(".button-add").addEventListener("click", function() {
+    number ++;
+    document.querySelector(".result").textContent = number;
+ColorChange()
+}); 
+
+document.querySelector(".button-remove").addEventListener("click", function() {
+    number --;
+    document.querySelector(".result").textContent = number;
+ColorChange()
+}); 
+
+function ColorChange(){
+    if (number < 10) {
+        document.querySelector(".result").style.color = "red";
+    } else if (number >= 10 && number <20 ) {
+        document.querySelector(".result").style.color = "orange";
+    } else document.querySelector(".result").style.color = "green";
+}
